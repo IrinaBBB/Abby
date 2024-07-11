0155c5079a4b714eeb72feea5ff00bcf08768e7b
IrinaBBB <balkarovairina@gmail.com>
2024-07-11 15:33:12 +0200

    Add Initial Commit

---
+++ AbbyWeb/Model/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        
+        public string Name { get; set; }
+
+        [Display(Name="Display Order")]
+        [Required(ErrorMessage = "Display order cannot be empty")]
+        [Range(1, int.MaxValue, ErrorMessage = "Custom error message: The value must be a valid integer.")]
+        public int? DisplayOrder { get; set; }
+    }
+}
